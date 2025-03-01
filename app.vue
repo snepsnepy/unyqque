be39baa3c631ec2015e2026d911600db2da4edf7
snepsnepy <snepysneptest@gmail.com>
2025-03-01 16:00:55 +0200

    First commit

---
+++ app.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>UNYQQUE</div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@pinia/nuxt", "@vueuse/nuxt", "@nuxt/image", "@nuxt/fonts"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});

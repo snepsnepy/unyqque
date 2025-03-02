4c64bad2a00abef8be4101388e4c3585d38deba0
msavulescu <msavulescu@wallbid.io>
2025-03-02 11:23:44 +0200

    Used NuxtImg

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   <section class="flex flex-col min-h-screen justify-between container mx-auto">
     <Navigation />
     <div class="flex w-full justify-center">
-      <img
+      <NuxtImg
         src="@/assets/img/logo.svg"
         alt="unyqque logo"
-        class="w-[400px] h-[200px] md:w-[600px] md:h-[600px]"
+        class="w-[400px] md:w-[600px] md:h-[600px]"
       />
     </div>
     <div class="text-center mb-10">

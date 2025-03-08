4fb874013cda961b4fd16be709e9baf65261951e
msavulescu <msavulescu@wallbid.io>
2025-03-08 22:28:17 +0200

    Added products cart modal.

---
+++ components/ProductsCart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="relative py-2 text-right">
+    <div class="-top-1.5 absolute right-0">
+      <p
+        v-if="shoppingCart.length"
+        class="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white"
+      >
+        {{ shoppingCart.length }}
+      </p>
+    </div>
+    <Icon name="solar:cart-outline" class="w-8 h-8 mr-3 text-neutral" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { type ShopItem } from "@/types/index";
+
+defineProps<{
+  shoppingCart: Array<ShopItem>;
+}>();
+</script>
---
+++ components/ProductsCartModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <dialog class="modal modal-open">
+    <div
+      class="modal-box bg-white/50 p-4 md:p-6 rounded-3xl bg-clip-padding backdrop-filter backdrop-blur-md"
+    >
+      <div method="dialog">
+        <button
+          @click="emit('closeIconClicked')"
+          class="btn btn-sm btn-circle btn-ghost text-neutral hover:text-white hover:bg-neutral/10 hover:border-none absolute right-4 top-4"
+        >
+          ✕
+        </button>
+      </div>
+
+      <div class="flex flex-col gap-y-4 md:gap-y-4 pt-8">
+        <h1>PRODUCT CART</h1>
+        <div
+          class="flex flex-col gap-y-2"
+          v-for="(item, index) in shoppingCart"
+          :key="index"
+        >
+          <p>{{ item.name }}</p>
+          <p>{{ item.price }}</p>
+          <p>{{ item.size }}</p>
+        </div>
+      </div>
+    </div>
+    <form method="dialog" class="modal-backdrop bg-black/70">
+      <button>close</button>
+    </form>
+  </dialog>
+</template>
+
+<script lang="ts" setup>
+import { type ShopItem } from "@/types/index";
+
+const emit = defineEmits(["closeIconClicked"]);
+
+defineProps<{
+  shoppingCart: Array<ShopItem>;
+}>();
+</script>

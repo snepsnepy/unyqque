dc97e05107a8eab57b8b82cf6c07a1aa26dbba62
msavulescu <msavulescu@wallbid.io>
2025-05-03 13:34:49 +0300

    Updated logic to get the design colors from the database

--- components/ProductModal.vue
+++ components/ProductModal.vue
@@ -49,7 +49,7 @@
           </div>
 
           <Dropdown
-            :items="designColor"
+            :items="designColors!"
             :placeholder="'Select a design color'"
             :is-design-color-picker="true"
             v-model:product-color="store.selectedItem!.designColor"
@@ -98,10 +98,12 @@
 <script lang="ts" setup>
 const emit = defineEmits(["closeIconClicked"]);
 const store = useShopStore();
+const { selectedItem } = storeToRefs(store);
+const { $supabase } = useNuxtApp();
 
 const sizes = ["2XS", "XS", "S", "M", "L", "XL", "2XL", "3XL"];
 const colors = ["Black", "White"];
-const designColor = ["Green", "Pink", "Blue"];
+const designColors = ref<string[]>(selectedItem.value?.designcolors!);
 
 const addItemAndClose = () => {
   store.addToCart(store.selectedItem!);
@@ -111,8 +113,6 @@ const addItemAndClose = () => {
 const hasSelectedValues = computed(
   () => store.selectedItem?.size && store.selectedItem?.color
 );
-
-const { $supabase } = useNuxtApp();
 
 const imageUrl = computed(() => {
   let fileName = "";

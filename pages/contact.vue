6f30516808b27ece1d32f010586208b1840e0a0a
snepsnepy <snepysneptest@gmail.com>
2025-05-17 14:50:42 +0300

    Fixed responsiveness in Contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,17 +1,19 @@
 <template>
-  <section class="flex flex-col flex-grow gap-8 h-dvh n container mx-auto">
-    <div class="flex flex-col items-center justify-center gap-4 w-full h-full">
+  <section
+    class="flex flex-col flex-grow gap-8 h-dvh n container mx-auto justify-center"
+  >
+    <div class="flex flex-col items-center justify-center gap-4 w-full">
       <div
         class="flex flex-col md:flex-row justify-between items-center text-center gap-2 w-2/3"
       >
         <p
-          class="text-neutral text-lg md:text-4xl font-extrabold font-montserrat whitespace-nowrap"
+          class="text-neutral text-2xl md:text-4xl font-extrabold font-montserrat whitespace-nowrap"
         >
           PHONE:
         </p>
         <a
           href="tel:+40738950590"
-          class="text-neutral text-lg md:text-4xl font-montserrat font-semibold"
+          class="text-neutral text-xl md:text-4xl font-montserrat font-semibold"
         >
           +40 738 950 590
         </a>
@@ -21,14 +23,14 @@
         class="flex flex-col md:flex-row justify-between items-center text-center gap-2 w-2/3"
       >
         <p
-          class="text-neutral text-lg md:text-4xl font-extrabold font-montserrat whitespace-nowrap"
+          class="text-neutral text-2xl md:text-4xl font-extrabold font-montserrat whitespace-nowrap"
         >
           PERSONAL INSTAGRAM:
         </p>
 
         <LinkPreview url="https://www.instagram.com/nyqqu/">
           <p
-            class="text-neutral text-lg md:text-4xl font-montserrat font-semibold"
+            class="text-neutral text-xl md:text-4xl font-montserrat font-semibold"
           >
             @nyqqu
           </p>
@@ -39,13 +41,13 @@
         class="flex flex-col md:flex-row justify-between items-center text-center gap-2 w-2/3"
       >
         <p
-          class="text-neutral text-lg md:text-4xl font-extrabold font-montserrat whitespace-nowrap"
+          class="text-neutral text-2xl md:text-4xl font-extrabold font-montserrat whitespace-nowrap"
         >
           SHOP INSTAGRAM:
         </p>
         <LinkPreview url="https://www.instagram.com/unyqque/">
           <p
-            class="text-neutral text-lg md:text-4xl font-montserrat font-semibold"
+            class="text-neutral text-xl md:text-4xl font-montserrat font-semibold"
           >
             @unyqque
           </p>
@@ -56,13 +58,13 @@
         class="flex flex-col md:flex-row justify-between items-center text-center gap-2 w-2/3"
       >
         <p
-          class="text-neutral text-lg md:text-4xl font-extrabold font-montserrat whitespace-nowrap"
+          class="text-neutral text-2xl md:text-4xl font-extrabold font-montserrat whitespace-nowrap"
         >
           FACEBOOK:
         </p>
         <LinkPreview url="https://www.facebook.com/nyqqu">
           <p
-            class="text-neutral text-lg md:text-4xl font-montserrat font-semibold"
+            class="text-neutral text-xl md:text-4xl font-montserrat font-semibold"
           >
             @unyqque
           </p>

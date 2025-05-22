b0f2086d220a4b64e4512834afd82f5e32d6d332
msavulescu <msavulescu@wallbid.io>
2025-05-22 21:35:20 +0300

    Updated font

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     class="flex flex-col items-center justify-end gap-y-8 py-2 md:py-6 h-full"
   >
     <div
-      class="flex flex-col items-center md:flex-row gap-x-2 font-montserrat text-lg leading-5 md:text-4xl text-neutral font-semibold"
+      class="flex flex-col items-center md:flex-row gap-x-2 font-delight text-lg leading-5 md:text-4xl text-neutral font-semibold"
     >
       <span class="whitespace-nowrap text-base-content">AUTOMOTIVE DESIGN</span>
       <span class="text-primary">&</span>

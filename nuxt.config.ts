19b33f0dc71a5e5b3b70020a8e8bb57a28ac2a40
snepsnepy <snepysneptest@gmail.com>
2025-07-02 19:37:15 +0300

    Updated favicon and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  app: {
+    head: {
+      title: "UNYQQUE",
+    },
+  },
   devtools: { enabled: true },
   ssr: false,
   css: ["@/assets/css/main.css"],

e17cf104bac7b4ec3b2436552fa3a3843bce066d
snepsnepy <snepysneptest@gmail.com>
2025-07-26 13:00:12 +0300

    Updated bg image and hero text

--- pages/index.vue
+++ pages/index.vue
@@ -4,26 +4,24 @@
     <div class="flex-1"></div>
 
     <!-- Main content centered -->
-    <section class="flex flex-row items-center justify-between">
+    <section
+      class="flex flex-col gap-y-8 md:flex-row items-center justify-between"
+    >
       <img
         src="@/assets/img/rx7-bg.png"
         alt="Hero image"
         class="w-2/3 object-contain"
       />
-      <ClientOnly>
-        <BlurReveal :delay="0.2" :duration="0.75" class="p-8 w-full h-full">
-          <h2
-            class="text-3xl font-bold tracking-tighter xl:text-6xl/none sm:text-5xl"
-          >
-            Hey there 👋
-          </h2>
-          <span
-            class="text-pretty text-xl tracking-tighter xl:text-4xl/none sm:text-3xl"
-          >
-            How is it going?
-          </span>
-        </BlurReveal>
-      </ClientOnly>
+      <h1
+        class="text-lg leading-5 md:text-5xl font-delight font-medium text-base-content text-center"
+      >
+        get yourself <br />
+        the best <br />
+        <span class="font-black">AUTOMOTIVE</span> <br />
+        merch <br />
+        and be <br />
+        <span class="font-black">UNYQQUE</span>
+      </h1>
     </section>
 
     <!-- Spacer to push footer to bottom -->
